daffaf3d23dfe85e0abada9e259b97fa0cd5cb74
Abhishek-040199 <90102336+Abhishek-040199@users.noreply.github.com>
2021-10-01 13:45:58 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -18,21 +18,26 @@
     </h4>
     <script>
     var p1 = 'http://52.66.204.194:8000/github/Enterprise';
+    cosole.log(p1);
     </script>
      
     <?php
     	$domain="<script>document.writeln(p1);</script>";
     	include("simple_html_dom.php");
-
+		echo "hello Printing php";
+        echo $domain;
 		$html = file_get_html($domain);
 
 		$final=$html->plaintext;
+        echo $final;
+        echo "end of php";
     ?>
   
     <script type="text/javascript">
         var x = "<?php echo"$final"?>";
         document.write(x);
-        console.log(x);
+        console.log("Javascript"+x);
+        console.log("Hiiiii");
     </script>
 </body>
   
